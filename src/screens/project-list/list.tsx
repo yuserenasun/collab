50df87eab174302fa70c1fc88f9f3cdeefbc8a8c
Yu Sun <pkusunyu@gmail.com>
2022-04-13 10:58:26 -0400

    ci: any

---
+++ src/screens/project-list/list.tsx
@@ -0,0 +1,38 @@
+import { User } from "./search-panel";
+
+interface Project {
+  id: string;
+  name: string;
+  personId: string;
+  pin: boolean;
+  organization: string;
+}
+
+interface ListProps {
+  list: Project[];
+  users: User[];
+}
+
+export const List = ({ users, list }: ListProps) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Task</th>
+          <th>Team Leader</th>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((project) => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>
+              {users.find((user) => user.id === project.personId)?.name ||
+                "Unknown"}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,27 +1,29 @@
 import { useEffect, useState } from "react";
 
 // deal with when value is 0
-export const isFalse = (value) => (value === 0 ? false : !value);
+export const isFalse = (value: unknown) => (value === 0 ? false : !value);
 // double "!": return boolean of a value
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalse(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     // set a timeout everytime value changes
